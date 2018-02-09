7442df674e3d229d89f8235ead71dc6212ba260d
Jules Desjardin <oranjules@hotmail.fr>
2018-02-09 17:14:59 +0100

    Added keyboard support

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <Keypad.h>
 
 HardwareSerial Serial2(2);
 
@@ -8,6 +9,21 @@ char nextChar()
   while(!Serial2.available());
   return Serial2.read();
 }
+
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {18,5,4,15}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {23,22,21,19}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
 void setup() {
 
   Serial.begin(115200);
@@ -33,4 +49,9 @@ void loop() {
       }
     }
   }
+  char key = keypad.getKey();
+
+if (key){
+  Serial.println(key);
+}
 }
