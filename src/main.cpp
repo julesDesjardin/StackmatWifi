5dce62bea108047c12b6723c2480fc5f31e7cab1
Jules <oranjules@hotmail.fr>
2018-06-14 14:20:10 +0200

    New pin assignment for M5stack

--- src/main.cpp
+++ src/main.cpp
@@ -21,8 +21,8 @@ char keys[ROWS][COLS] = {
   {'7','8','9','C'},
   {'*','0','#','D'}
 };
-byte rowPins[ROWS] = {18,5,4,15}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {23,22,21,19}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = {18,19,23,22}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {16,17,2,5}; //connect to the column pinouts of the keypad
 
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
@@ -30,7 +30,7 @@ Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 int getTime()
 {
-  int ret;
+  int ret = 0;
   ret += (nextChar()-'0')*6000;
   Serial.println(ret);
   ret += (nextChar()-'0')*1000;
@@ -48,7 +48,7 @@ void setup() {
   Serial.begin(115200);
 
   // Serial2.begin(unsigned long baud, uint32_t config, int8_t rxPin, int8_t txPin, bool invert)
-  Serial2.begin(1200, SERIAL_8N1, 26, 25, true);
+  Serial2.begin(1200, SERIAL_8N1, 21, 25, true);
 }
 
 void loop() {
@@ -65,7 +65,7 @@ void loop() {
   Serial2.flush();
   while(!stopped)
   {
-    Serial.println("Running");
+    //Serial.println("Running");
     if(Serial2.available())
     {
       int ch = Serial2.read();
